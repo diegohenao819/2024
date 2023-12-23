a91f483604e3714570d82ece5ed209e8faceffda
diegohenao819 <diegohenao819@gmail.com>
2023-12-23 00:20:51 -0500

    delete

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: './App.jsx' // Ensure this is the path to your entry HTML file
-    }
+    },
+    input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs'
+  },
   }
 });
