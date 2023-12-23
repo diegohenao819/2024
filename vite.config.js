6710f333592669cb0b575ab2d71d4e4f59cee431
diegohenao819 <diegohenao819@gmail.com>
2023-12-22 22:58:06 -0500

    rollup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://diegohenao819.github.io/github-pages",
+  base: "/2024/",
+  build: {
+    rollupOptions: {
+      input: './index.html' // Ensure this is the path to your entry HTML file
+    }
+  }
 });
