2f832137f0400afda6b37c11ef6db9b20642529a
diegohenao819 <diegohenao819@gmail.com>
2023-12-23 16:02:56 -0500

    vite.confing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import svgr from "vite-plugin-svgr";
 export default defineConfig({
-  entry: "./index.html",
-  plugins: [react()],
+  entry: "./build/index.html",
+  plugins: [react(), tsconfigPaths(), svgr()],
+  server: {
+    port: 3000,
+    host: true,
+    open: "/",
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("index.html"),
+      },
+    },
+    commonjsOptions: {
+      include: [".yarn/**"],
+    },
+  },
+  cacheDir: "./.vite",
+  define: {
+    global: "globalThis",
+  },
 });
