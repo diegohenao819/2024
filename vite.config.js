f677cd21f0fda18befae66d1e2245588e44d2731
diegohenao819 <diegohenao819@gmail.com>
2023-12-23 00:25:56 -0500

    erwerwe rwer wer rwe rwer

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/2024/",
-  build: {
-    rollupOptions: {
-      input: './App.jsx' // Ensure this is the path to your entry HTML file
-    },
-    input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  },
-  }
 });
