4e3bc18bb4ee871f08b0ddd721355e8dc9de921d
diegohenao819 <diegohenao819@gmail.com>
2023-12-22 22:38:12 -0500

    ultimos cambios

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/2024/",
-
-  build: {
-    rollupOptions: {
-      input: "./index.html", // Ensure this is the path to your entry HTML file
-    },
-  },
+  plugins: [react()],
 });
